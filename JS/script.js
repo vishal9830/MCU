73bfaf744546fff19a00a1f654d5c1b5010ac5ad
Vivek Chauhan <iamvivekchauhan19@gmail.com>
2024-03-14 19:49:14 -0400

    background color of sections on mouseover

--- JS/script.js
+++ JS/script.js
@@ -11,3 +11,25 @@ document.querySelectorAll('nav a').forEach(link => {
         this.style.color = '#fff';
     });
 });
+
+// Change background color of sections on mouseover and mouseout
+document.querySelectorAll('section').forEach(section => {
+    section.addEventListener('mouseover', function() {
+        this.style.backgroundColor = '#f2f2f2';
+    });
+    section.addEventListener('mouseout', function() {
+        this.style.backgroundColor = '#fff';
+    });
+});
+
+// Change font size of footer on double click
+document.querySelector('footer').addEventListener('dblclick', function() {
+    this.style.fontSize = '18px';
+});
+
+// Show alert when clicking on movie names
+document.querySelectorAll('section li').forEach(movie => {
+    movie.addEventListener('click', function() {
+        alert("You clicked on " + this.innerText);
+    });
+});
